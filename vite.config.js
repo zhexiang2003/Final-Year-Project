d06dadec481ecbd4804409d8d24094e716b9f9ff
Zhe Xiang <zhexiang2003@gmail.com>
2025-05-08 03:55:46 +0800

    Deleted Nuxt UI

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import ui from '@nuxt/ui/vite'
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), ui(), tailwindcss()],
+  plugins: [vue(), tailwindcss()],
 });
 
