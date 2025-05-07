73e9dcda2bd01d8d8d36f155d0eb1185925943dd
Zhe Xiang <zhexiang2003@gmail.com>
2025-05-08 04:42:00 +0800

    Test primevue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,7 @@ import Button from "primevue/button"
 
 <template>
   <div class="card flex justify-center">
-      <Button label="Verify" class="bg-[]" />
+    <Button label="Verify" class="!bg-[#FF0000] !border-0 !rounded-none" />
   </div>
 </template>
 
